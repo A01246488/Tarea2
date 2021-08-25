f4788a4e7f0c2b664cc454d0426dddbe38bc2173
Ernesto Flores Velo <89033350+A01246488@users.noreply.github.com>
2021-08-24 23:34:47 -0600

    Update exercise.py

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,7 +1,19 @@
-
 def main():
-    pass
-
-
+    Edad=int(input("Ingresa tu edad:"))
+    if Edad < 18:
+        print("No cumples con los requisitos")
+    if Edad > 17:
+        while True:
+            ID = input("¿Tienes identificación oficial? " "(s/n): ")
+            if ID == "n":
+                print("No cumple con los requisitos")
+                break
+            elif ID == "s":
+                print("Trámite de licencia concedido")
+                break
+            else:
+                print("Respuesta incorrecta")
+                break
+        
 if __name__ == '__main__':
     main()
